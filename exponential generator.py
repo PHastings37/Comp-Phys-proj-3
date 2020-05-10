b82553b95792f19246672d44a6a2bed6e42cb49c
PHastings37 <patrickhastings@hotail.co.uk>
2020-05-10 13:09:34 +0100

    starting absorption with random walks, still issues with errors in exp gen

--- exponential generator.py
+++ exponential generator.py
@@ -12,15 +12,33 @@ import matplotlib.pyplot as plt
 x = np.random.uniform(0, 1, size =5000)
 
 lmda = 0.45
-y = np.exp(-x / lmda)
+y = -lmda * np.log(x)
     
-num_bins = 40
+num_bins = 100
 fig = plt.figure()
-n, bins, patches = plt.hist(y, num_bins, facecolor='blue', alpha=0.5)
+plt.hist(y, num_bins, facecolor='blue', alpha=0.5)
 plt.show()
 
-fit = np.polyfit(x, np.log(np.abs(y)), deg=1, cov=False)
+[n, xs] = np.histogram(y, num_bins)
+xs = xs[:-1]
+
+
+
+ni = np.array([])
+xi = np.array([])
+
+for i in range(len(n)):
+    if n[i] != 0:
+        ni = np.append(ni, n[i])
+        xi = np.append(xi, xs[i])
+
+err_n = np.sqrt(len(ni) * (1 - len(ni)/len(x))) / len(ni)
+err_logn = err_n * np.log(ni)
+fit = np.polyfit(xi, np.log(ni), deg=1, w=err_logn, cov=False)
+
 plt.figure()
-plt.scatter(x, fit[0] * x + fit[1])
+plt.errorbar(xi, np.log(ni), yerr=err_logn, linestyle='None')
+plt.scatter(xi, np.log(ni), marker='x')
 plt.show()
+
 print(-1/fit[0])
---
+++ random walk with absorption.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 10 13:07:35 2020
+
+@author: Patrick
+"""
+
